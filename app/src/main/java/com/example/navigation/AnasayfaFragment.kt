a0f1870bf7dce8ad4e6a1b9c4f3d8912bee4f70f
tarikkeskin <tarik.keskin007@gmail.com>
2022-06-18 01:30:03 +0300

    navigation

---
+++ app/src/main/java/com/example/navigation/AnasayfaFragment.kt
@@ -0,0 +1,29 @@
+package com.example.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_anasayfa.*
+import kotlinx.android.synthetic.main.fragment_anasayfa.view.*
+
+
+class AnasayfaFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val tasarim = inflater.inflate(R.layout.fragment_anasayfa, container, false)
+
+        tasarim.textView.text = "Helloo"
+
+        tasarim.buttonOyun.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.oyunEkraninaGecis)
+        }
+
+        return tasarim
+    }
+
+}
